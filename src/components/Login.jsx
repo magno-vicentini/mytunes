cefeef740d96c293dac3a9e08468c15b24100057
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2021-10-19 14:39:43 -0300

    requisito 1

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { createUser } from '../services/userAPI';
 import Loading from './Loading';
 
 class Login extends Component {
   render() {
     const { name, onInputChange } = this.props;
-    const MINLENGTH = 3;
+    const MINLENGTH = 2;
     return (
       <div data-testid="page-login">
         <p>TrybeTunes</p>
@@ -23,7 +24,10 @@ class Login extends Component {
             data-testid="login-submit-button"
             disabled={ name.length <= MINLENGTH }
             onClick={ async () => {
+              <Loading />;
               await createUser({ name });
+
+              return <Redirect to="/search" />;
             } }
           >
             Entrar
