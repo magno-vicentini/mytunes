3b003bbd8a9014c0b9e7209c5954f298a493e4f5
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2021-10-20 20:14:26 -0300

    requisito 6

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+// import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     id: '',
+  //   };
+  // }
+
+  // getMusicsId = () => {
+  //   const { match } = this.props;
+  //   const { id } = match.params;
+  //   this.setState({ id });
+  //   // const musics = await getMusics(id);
+  //   // console.log(musics);
+  //   console.log(id);
+  // }
+
   render() {
     return (
       <div data-testid="page-album">
         <Header />
-        <div>Album</div>
+        <div>alou</div>
       </div>
     );
   }
