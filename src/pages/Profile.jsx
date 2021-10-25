3749ba16754a93d9ff3495e767d778257c240a25
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2021-10-24 22:04:11 -0300

    requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import { getUser } from '../services/userAPI';
@@ -24,7 +25,10 @@ class Profile extends Component {
   }
 
   render() {
-    const { loadingProfile, profile: { name, image, description, email } } = this.state;
+    const { loadingProfile,
+      profile: { name, image, description, email },
+      profile } = this.state;
+    if (!profile) return <Loading />;
     if (loadingProfile) {
       return (
         <div>
@@ -36,25 +40,26 @@ class Profile extends Component {
     return (
       <div data-testid="page-profile">
         <Header />
-        <div>
+        { name }
+        <div className="profile-card">
+          <label htmlFor="nameUser">
+            Nome
+            <span>{ name }</span>
+          </label>
+          <label htmlFor="email">
+            E-mail:
+            <p>{email}</p>
+          </label>
+          <label htmlFor="description">
+            Descrição:
+            <p id="description">{description}</p>
+          </label>
           <div>
             <img data-testid="profile-image" src={ image } alt="" />
             <Link to="/profile/edit">
               <button type="button">Editar perfil</button>
             </Link>
           </div>
-          <label htmlFor="name">
-            Nome:
-            <p id="name">{name}</p>
-          </label>
-          <label htmlFor="email">
-            E-mail:
-            <p>{`${email}@trybetunes.com`}</p>
-          </label>
-          <label htmlFor="description">
-            Descrição:
-            <p id="description">{`Descrição: ${description}`}</p>
-          </label>
         </div>
       </div>
     );
